ed03acf5a1fb8b9995880b32e30bb0f9ca4602ce
wy <228681163@qq.com>
2023-08-13 23:30:46 +0800

    注册发送验证码邮件，验证码存入redis

--- send_services/src/main/java/com/mingyue/sendmsg/services/EmailService.java
+++ send_services/src/main/java/com/mingyue/sendmsg/services/EmailService.java
@@ -1,9 +1,14 @@
 package com.mingyue.sendmsg.services;
 
+import jakarta.mail.internet.MimeMessage;
 import org.apache.log4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.ServletRequestUtils;
 import redis.clients.jedis.Jedis;
@@ -11,6 +16,7 @@ import redis.clients.jedis.JedisPool;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Date;
 import java.util.Random;
 
 @Service
@@ -18,8 +24,22 @@ public class EmailService {
 
     public Logger logger = Logger.getLogger(this.getClass());
 
+    @Value("${spring.mail.jndi-name}")
+    private String sendName;
+
+    public String getSendName() {
+        return sendName;
+    }
+
+    public void setSendName(String sendName) {
+        this.sendName = sendName;
+    }
+
     @Autowired
     private JedisPool jedisPool;
+
+    @Autowired
+    private JavaMailSender mailSender;
 
     public void getCode(HttpServletRequest request) throws ServletRequestBindingException {
 
@@ -38,12 +58,39 @@ public class EmailService {
 
         Jedis jedis = jedisPool.getResource();
         // 有效期5分钟
-        jedis.setex(session.getId(),(1000L * 60 * 5),code.toString());
+        jedis.setex(session.getId(),(60L * 5),code.toString());
+
+        logger.warn("sendName" + this.sendName);
+        sendSimpleMail(email,this.sendName,"明月的小屋-验证码","您的验证码：" + code.toString() + "，有效期为5分钟");
 
     }
 
-    private void sendEmail() {
+    public void sendSimpleMail(String to,String sendName,String subject,String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        //邮件发件人
+        message.setFrom(sendName);
+        //邮件收件人 1或多个
+        message.setTo(to.split(","));
+        //邮件主题
+        message.setSubject(subject);
+        //邮件内容
+        message.setText(content);
+        checkMail(message);
+        //邮件发送时间
+        message.setSentDate(new Date());
+
+
+        mailSender.send(message);
 
     }
+
+
+    public void checkMail(SimpleMailMessage mailRequest) {
+        Assert.notNull(mailRequest,"邮件请求不能为空");
+        Assert.notNull(mailRequest.getTo(), "邮件收件人不能为空");
+        Assert.notNull(mailRequest.getSubject(), "邮件主题不能为空");
+        Assert.notNull(mailRequest.getText(), "邮件收件人不能为空");
+    }
+
 
 }
